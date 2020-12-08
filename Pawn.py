b49fca6294f72bc99b3395077a9128eae48d6c4a
UlisesE88 <ulisesespino88@gmail.com>
2020-12-08 12:24:40 -0500

    Simplified Code

--- Pawn.py
+++ Pawn.py
@@ -1,3 +1,5 @@
+import operator
+
 class Pawn:
 
     def __init__(self, r, c, color):
@@ -13,60 +15,47 @@ class Pawn:
         valid_moves = []
         r = self.r
         c = self.c
+        my_color = self.color
 
-        if self.color == 'W':
-            #At the start
-            if r == 6:
-                if board[r - 1][c] is None:
-                    valid_moves.append((r - 1, c))
-                if board[r - 2][c] is None:
-                    valid_moves.append((r - 2, c))
-            #Anywhere on the board
-            if r < 6:
-                if board[r - 1][c] is None:
-                    valid_moves.append((r - 1, c))
-            #Taking a piece
-            if board[r - 1][c-1] is not None and board[r - 1][c-1].color == 'B':
-                valid_moves.append((r - 1, c-1))
+        x = 0
+        first_row = -1
+        if my_color == 'W': x, first_row, last_row = -1, 6, 0
+        if my_color == 'B': x, first_row, last_row = 1, 1, 7
+        comp = operator.lt if my_color == 'W' else operator.gt
 
-            if board[r - 1][c+1] is not None and board[r - 1][c+1].color == 'B':
-                valid_moves.append((r - 1, c+1))
+        #If at the end of the board
+        if r == last_row: return valid_moves
 
-        if self.color == 'B':
-            #At the start
-            if r == 1:
-                if board[r + 1][c] is None:
-                    valid_moves.append((r + 1, c))
-                if board[r + 2][c] is None:
-                    valid_moves.append((r + 2, c))
-            #Anywhere on the board
-            if r > 1:
-                if board[r + 1][c] is None:
-                    valid_moves.append((r + 1, c))
-            #Taking a piece
-            if board[r + 1][c-1] is not None and board[r+1][c-1].color == 'W':
-                valid_moves.append((r+1, c-1))
+        # At the start
+        if r == first_row:
+            #Move one space
+            if board[r + x][c] is None: valid_moves.append((r + x, c))
+            #Move two spaces
+            if board[r + (2 * x)][c] is None: valid_moves.append((r + (2 * x), c))
 
-            if board[r+1][c+1] is not None and board[r+1][c+1].color == 'W':
-                valid_moves.append((r+1, c+1))
+        #Anywhere on the board
+        if comp(r, first_row):
+            #Move one space
+            if board[r + x][c] is None: valid_moves.append((r + x, c))
+
+        #Taking a piece
+        if board[r + x][c-1] is not None and board[r + x][c-1].color != my_color:
+            valid_moves.append((r + x, c-1))
+        if board[r + x][c+1] is not None and board[r + x][c+1].color != my_color:
+            valid_moves.append((r + x, c+1))
 
         return valid_moves
 
-    def moving(self, r, c, board):
+    def moving(self, new_r, new_c, board):
         item_ate = None
+        old_r, old_c  = self.r, self.c
 
-        old_r = self.r
-        old_c = self.c
+        if board[new_r][new_c] is not None: item_ate = board[new_r][new_c]
 
-        if board[r][c] is not None:
-            item_ate = board[r][c]
-
-        board[r][c] = self
+        board[new_r][new_c] = self
         board[old_r][old_c] = None
 
-        self.r = r
-        self.c = c
-
+        self.r, self.c = new_r, new_c
         return (board, item_ate)
 
 if __name__ == '__main__':
@@ -83,10 +72,14 @@ if __name__ == '__main__':
         print(row)
 
 
-    print(w_pawn.get_moves(board))
-    (board, item_ate) = w_pawn.moving(5, 0, board)
+    print(b_pawn.get_moves(board))
+    (board, item_ate) = w_pawn.moving(5, 1, board)
 
+    print()
     for row in board:
         print(row)
 
-    print(item_ate.piece + ' ' +  item_ate.color)
+    if item_ate is not None:
+        print(item_ate.piece + ' ' +  item_ate.color)
+
+
