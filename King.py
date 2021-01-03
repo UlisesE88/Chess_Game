a57a53fd389bb470f9714e368f0c05c4392fd2d0
JohanNinoEspino <johanninoespino88@gmail.com>
2021-01-02 23:54:59 -0500

    King and Queen

---
+++ King.py
@@ -0,0 +1,15 @@
+#import operator
+class King:
+
+    def __init__(self, r, c, color):
+        self.r = r
+        self.c = c
+        self.color = color
+        self.name = 'King'
+
+    def get_position(self):
+        return (self.r, self.c)
+
+    def get_moves(self, board):
+        valid_moves = []
+        r, c, my_color = self.r, self.c, self.color
---
+++ Queen.py
@@ -0,0 +1,15 @@
+#import operator
+class Queen:
+
+    def __init__(self, r, c, color):
+        self.r = r
+        self.c = c
+        self.color = color
+        self.name = 'Queen'
+
+    def get_position(self):
+        return (self.r, self.c)
+
+    def get_moves(self, board):
+        valid_moves = []
+        r, c, my_color = self.r, self.c, self.color
